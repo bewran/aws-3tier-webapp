74431e4f48176ab1024cc700634780d7c4ffa830
Bewran <bewran@windowslive.com>
2025-04-25 13:29:01 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello from the Backend API!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
