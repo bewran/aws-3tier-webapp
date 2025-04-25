5f57b9e14bcaa90d2a506f1a2adad185f8fed2df
Bewran <bewran@windowslive.com>
2025-04-25 13:27:33 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,28 @@
+provider "aws" {
+  region = "us-west-2"  # Update to your region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-0abcdef1234567890"  # Change to correct AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "Frontend-Server"
+  }
+}
 
